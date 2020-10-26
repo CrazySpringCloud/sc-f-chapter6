6fb939795969221483f1ec54ff931cdd8df01bfd
danieldong0802 <1756330108@qq.com>
2020-10-26 12:52:17 +0800

    [MOD]提交中间代码

--- config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/forezp/configclient/ConfigClientApplication.java
@@ -1,20 +1,18 @@
-package com.forezp.configserver;
+package com.forezp.configclient;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(ConfigClientApplication.class);
 
     public static void main(String[] args) {
         logger.info("Spring Cloud 配置中心服务 config-server begin to start.....");
-        SpringApplication.run(ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigClientApplication.class, args);
         logger.info("Spring Cloud 配置中心服务 config-server started.....");
     }
 
---
+++ config-client/src/main/java/com/forezp/configclient/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.forezp.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author dong
+ * @Date 2020/10/25
+ * @Version 1.0
+ **/
+@RestController
+public class HelloController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}
